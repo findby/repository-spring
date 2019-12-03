a8c571ce4a9fecc0b4b188322fdd93260f64235b
findby <37327767+findby@users.noreply.github.com>
2019-12-03 10:20:53 +0800

    添加注释

--- part-parent/part-spring/src/main/java/org/part/spring/commutils/JdbcTemplateUtils.java
+++ part-parent/part-spring/src/main/java/org/part/spring/commutils/JdbcTemplateUtils.java
@@ -2,7 +2,11 @@ package org.part.spring.commutils;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
+/***
+ * @author	wenxiang.fei
+ * @Description	统一使用JdbcTemplate
+ * @time	2019-12-03 10:12:09
+ */
 public class JdbcTemplateUtils {
 	
 	public static JdbcTemplate getSpringJdbcTemplate(JdbcTemplate jdbcTemplate) {
