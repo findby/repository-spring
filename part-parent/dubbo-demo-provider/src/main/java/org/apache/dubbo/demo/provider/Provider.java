549f91fac9727582d2037ebee17a45770a384af7
findby <37327767+findby@users.noreply.github.com>
2019-12-05 14:18:58 +0800

    修改

---
+++ part-parent/dubbo-demo-provider/src/main/java/org/apache/dubbo/demo/provider/Provider.java
@@ -0,0 +1,14 @@
+package org.apache.dubbo.demo.provider;
+
+
+
+public class Provider {
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-provider.xml"});
+        context.start();
+        System.out.println("Provider started.");
+        System.in.read(); // press any key to exit
+    }
+}
